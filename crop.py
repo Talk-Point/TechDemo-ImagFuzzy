fbaa25f8f6c606eda466e737309d847ab6fd578f
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-16 10:29:35 +0200

    init

---
+++ crop.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageChops
+
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
+im = Image.open("10034219_HTC-Desire-610-navbl_1.jpg")
+im = trim(im)
+im.show()
